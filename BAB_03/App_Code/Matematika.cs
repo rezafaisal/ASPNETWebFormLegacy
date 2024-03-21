eb6d88a4fa4aab8e40dcb082afa6af4067aea0b4
M Reza Faisal <reza.faisal@gmail.com>
2024-03-21 16:21:46 +0800

    update

---
+++ BAB_03/App_Code/Matematika.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BAB_03.App_Code
+{
+    public class Matematika
+    {
+        public int Tambah(int bil1, int bil2)
+        {
+            int hasil = bil1 + bil2;
+            return hasil;
+        }
+    }
+}
